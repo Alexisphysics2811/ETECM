9115e8b03b19e3fb534153117a2ee63fc6882923
Alexisphysics2811 <alexisphysics2811@outlook.com>
2022-10-10 13:12:45 -0500

    E4

--- src/Practica2CalculoDeSueldos.java
+++ src/Practica2CalculoDeSueldos.java
@@ -1,3 +1,37 @@
-public class Practica2CalculoDeSueldos {
+/***
+*Program: Practica1CasaDeCambio.java
+*Author: Alexis Rabago
+*Date: 30/10/2022
+***/
+
+//Lib
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Practica2CalculoDeSueldos extends JFrame implements ActionListener {
     
+    private JLabel labelWelcome;
+    private JTextField textField;
+    private JButton button;
+
+    public static void main(String[] args) {
+        Practica1CasaDeCambio demo = new Practica1CasaDeCambio();
+        demo.setSize(650, 500);
+        demo.createGUI();
+        demo.setTitle("Currency Converter");
+        demo.setVisible(true);
+    }
+
+    private void createGUI() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container window = getContentPane();
+        window.setLayout(new FlowLayout());
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Object o = e.getSource();
+        
+    }
 }
