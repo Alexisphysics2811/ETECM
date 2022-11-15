cd440b0c56c13bb6ffc5a941a1bed23c62f59bb7
Alexisphysics2811 <alexisphysics2811@outlook.com>
2022-11-14 23:21:01 -0600

    monke ig

--- src/Practica5Examen.java
+++ src/Practica5Examen.java
@@ -1,3 +1,29 @@
-public class Practica5Examen {
+/***
+*Program: Practica5Examen.java
+*Author: Alexis Rabago
+*Date: 14/11/2022
+***/
+
+//Lib
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Practica5Examen extends JFrame implements ActionListener{
     
+    public static void main(String[] args) {
+        Practica5Examen demo = new Practica5Examen();
+        demo.setSize(600,800);
+        demo.createGUI();
+        demo.setTitle("Examen Diagnostico");
+        demo.setVisible(true);
+    }
+
+    private void createGUI() {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
